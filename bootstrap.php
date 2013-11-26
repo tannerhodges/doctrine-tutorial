17d30db7ea5025ff52cf8a0e18bad04fe376cae9
Tanner Hodges <austintannerhodges@gmail.com>
2013-11-26 17:38:50 -0500

    Init bootstrap.php; Obtaining an EntityManager

---
+++ bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * bootstrap.php
+ * ---
+ * @see http://docs.doctrine-project.org/en/latest/tutorials/getting-started.html#obtaining-the-entitymanager
+ * @see http://docs.doctrine-project.org/en/latest/reference/configuration.html#obtaining-an-entitymanager
+ * @see http://docs.doctrine-project.org/en/latest/reference/advanced-configuration.html
+ */
+
+// Composer init
+require_once "vendor/autoload.php";
+
+// Doctrine libraries
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Settings
+ * ---
+ * $paths     = Path to Entity files
+ * $isDevMode = Determines caching behavior in the Setup helper
+ * 				@see vendor\doctrine\orm\lib\Doctrine\ORM\Tools\Setup::createConfiguration()
+ */
+$paths     = array(__DIR__."/src");
+$isDevMode = true;
+
+// Database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path'   => __DIR__ . '/db.sqlite',
+);
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+// and obtain the entity manager
+$config        = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($conn, $config);
+
+?>
