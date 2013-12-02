cb9b43b642aa1d5839cebc98550f3776fd98b026
Tanner Hodges <austintannerhodges@gmail.com>
2013-12-02 12:48:56 -0500

    Replace query logic with BugRepository methods in list_bugs

--- list_bugs.php
+++ list_bugs.php
@@ -8,23 +8,8 @@
 // Require your application's bootstrap file
 require_once "bootstrap.php";
 
-/**
- * Create DQL query to pull bugs from the database.
- * @see http://docs.doctrine-project.org/en/latest/reference/dql-doctrine-query-language.html
- * ---
- * @note The QueryBuilder is an alternative to handwriting DQL: $entityManager->createQueryBuilder()
- * @see http://docs.doctrine-project.org/projects/doctrine-mongodb-odm/en/latest/reference/query-builder-api.html#creating-a-query-builder
- */
-$dql = "SELECT b, e, r, p
-        FROM Bug b
-            JOIN b.engineer e
-            JOIN b.reporter r
-            JOIN b.products p
-        ORDER BY b.created DESC";
-
 // Perform query and save results in an array
-$query = $entityManager->createQuery($dql);
-$bugs = $query->getArrayResult();
+$bugs = $entityManager->getRepository('Bug')->getRecentBugsArray();
 
 // Loop through bugs and display
 foreach ($bugs AS $bug) {
